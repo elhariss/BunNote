19a5fece5e8842efa6575a74fee656a7c730ddaf
haris <elharith.040@gmail.com>
2026-02-04 14:27:04 +0100

    Update editorProvider.js

--- src/extension/editorProvider.js
+++ src/extension/editorProvider.js
@@ -37,7 +37,7 @@ class EditorProvider {
 
     webviewPanel.webview.html = this.getHtml(webviewPanel.webview);
 
-    const changeDocumentSubscription = vscode.workspace.onDidChangeTextDocument(e => {
+    const docChangeSub = vscode.workspace.onDidChangeTextDocument(e => {
       if (e.document.uri.toString() === document.uri.toString()) {
         webviewPanel.webview.postMessage({
           command: "updateContent",
@@ -119,7 +119,7 @@ class EditorProvider {
     });
 
     webviewPanel.onDidDispose(() => {
-      changeDocumentSubscription.dispose();
+      docChangeSub.dispose();
     });
   }
 
