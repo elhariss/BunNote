9992756702ec57e0b4c91b69172e03a11398213e
haris <elharith.040@gmail.com>
2026-02-05 19:50:48 +0100

    Add extension activation and command tests

--- test/extension.test.js
+++ test/extension.test.js
@@ -1,15 +1,21 @@
+
 const assert = require('assert');
-
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
 const vscode = require('vscode');
-// const myExtension = require('../extension');
 
-suite('Extension Test Suite', () => {
-	vscode.window.showInformationMessage('Start all tests.');
+suite('BunNote Extension Test Suite', () => {
+	test('Extension should be present', async () => {
+		const ext = vscode.extensions.getExtension('elharis.bunnote');
+		assert.ok(ext, 'Extension not found');
+	});
 
-	test('Sample test', () => {
-		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+	test('Extension activates', async () => {
+		const ext = vscode.extensions.getExtension('elharis.bunnote');
+		await ext.activate();
+		assert.ok(ext.isActive, 'Extension did not activate');
+	});
+
+	test('BunNote: Set Vault Folder command is registered', async () => {
+		const commands = await vscode.commands.getCommands(true);
+		assert.ok(commands.includes('bunnote.setVault'), 'bunnote.setVault command not found');
 	});
 });
